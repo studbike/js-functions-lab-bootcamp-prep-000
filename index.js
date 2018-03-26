333a6064404fd0d8a9f1de4dc98391f8d72526ee
grant diamond <grant.a.diamond@gmail.com>
2018-03-26 11:33:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo(name){
 function happyHolidayTo (name, holiday){
   return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown(days, holiday){
+function holidayCountdown(holiday, days){
   return `It's ${days} until ${holiday}!`
 }
